aae44a4ed286bd84d390524de0aa22a0ba25f6b1
Lin Chai <linchai@google.com>
2025-10-20 21:40:34 -0700

    [Tunix] Add qwen2.5 math-1.5b model config
    PiperOrigin-RevId: 821940898

--- tunix/models/qwen2/model.py
+++ tunix/models/qwen2/model.py
@@ -144,6 +144,21 @@ class ModelConfig:
         use_tied_embedding=True,
     )
 
+  @classmethod
+  def qwen2_5_math_1_5b(cls):  # qwen2.5-math-1.5B
+    return cls(
+        num_layers=28,
+        vocab_size=151936,
+        embed_dim=1536,
+        hidden_dim=8960,
+        num_heads=12,
+        head_dim=128,
+        num_kv_heads=2,
+        norm_eps=1e-06,
+        rope_theta=10000,
+        use_tied_embedding=True,
+    )
+
   # qwen2.5-coder-3B and qwen2.5-3B share the same config.
   @classmethod
   def qwen2_5_3b(cls):
